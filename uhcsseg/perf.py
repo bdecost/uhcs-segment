6b2b1465bfab3a5bd01e4d556ac0fdc18e566557
Brian DeCost <bdecost@andrew.cmu.edu>
2017-03-15 13:56:53 -0400

    add particle size distribution ks-test metric to perf.

--- uhcsseg/perf.py
+++ uhcsseg/perf.py
@@ -1,6 +1,8 @@
 """ segmentation performance metrics """
 
 import numpy as np
+import scipy as sp
+from skimage.measure import label, regionprops
 
 def accuracy(prediction, annotation):
     """ pixel-wise accuracy (percentage) """
@@ -20,3 +22,17 @@ def IU_avg(prediction, annotation, classlist=None):
         IU(prediction, annotation, cls) for cls in classlist
     ))
     return avg_IU
+
+def PSD(mask):
+    """ calculate empirical particle size distribution (in pixels) """
+    if np.unique(mask).size > 2:
+        raise NotImplementedError("PSD expects a binary segmentation mask")
+    return np.sqrt([region.area for region in regionprops(label(mask))])
+
+def ks_PSD(prediction, annotation):
+    """ Calculate particle size distributions and perform 2-sample ks test.
+        Metric is the ks test p-value
+        p < 0.05 -- reject 2-tailed null hypothesis --  distributions differ
+    """
+    ks = sp.stats.ks_2samp(PSD(annotation), PSD(prediction))
+    return ks.pvalue
