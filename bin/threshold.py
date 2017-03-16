23803f67cc68061aef7d3c60437c51f2f324810e
Brian DeCost <bdecost@andrew.cmu.edu>
2017-03-16 01:05:45 -0400

    add simple auto-threshold methods for particle segmentation.

---
+++ bin/threshold.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import h5py
+import click
+import numpy as np
+
+from skimage import filters
+
+import sys
+sys.path.append(os.getcwd())
+
+from uhcsseg import io
+@click.command()
+@click.argument('hfile', type=click.Path())
+@click.option('-r', '--resultsfile', default='data/sph_thresh.h5', type=click.Path())
+def threshold(hfile, resultsfile):
+
+    images, labels, keys = io.load_dataset(hfile)
+
+    for image, label, key in zip(images, labels, keys):
+
+        methods = filter(lambda f: 'threshold' in f, filters.__all__)
+        for method in methods:
+            if 'adaptive' in method:
+                # skip adaptive for now.
+                continue
+            
+            threshold = getattr(filters, method)
+
+        
+            # do threshold
+            pred = (image > threshold(image)).astype(int)
+        
+            # save results.
+            if os.path.isfile(resultsfile):
+                mode = 'r+'
+            else:
+                mode = 'w'
+
+            with h5py.File(resultsfile, mode) as f:
+                # save validation predictions
+                try:
+                    g = f[key]
+                except KeyError:
+                    g = f.create_group(key)
+                g[method] = pred
+
+            
+if __name__ == '__main__':
+    threshold()
+
