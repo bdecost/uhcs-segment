3f359b1dc07e38a1568e79c80f62fb50068aa372
Brian DeCost <bdecost@andrew.cmu.edu>
2017-05-02 12:28:03 -0400

    fix relative indexing based on coordinates: pass in h, w arguments -- spatial dimensions of tensor to be upsampled.

--- uhcsseg/upsample.py
+++ uhcsseg/upsample.py
@@ -7,12 +7,21 @@ import numpy as np
 import tensorflow as tf
 from keras import backend as K
 
-def get_values(inputs):
+def get_values(inputs, **arguments):
     """ index into input tensor x with indices in input tensor sel
     indices should explicitly contain the sample index: (b, i, j)
+    arguments: h, w -- dimensions of feature map to upsample
+        needed since dimensions are not known at model compile time
     """
-    x, sel = inputs
-    return tf.gather_nd(x, sel)
+    x, coords = inputs
+
+    # transform fractional coordinates to feature map coordinates
+    s1 = coords[:,:,1] * arguments['h']
+    s2 = coords[:,:,2] * arguments['w']
+    coords = tf.stack((tf.zeros_like(s1), s1, s2), 2)
+    indices = tf.cast(tf.round(coords), tf.int32)
+
+    return tf.gather_nd(x, indices)
 
 def sparse_upsample_output_shape(input_shape):
     data_shape, index_shape = input_shape
