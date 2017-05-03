93f1ff50bd6cc411e6461f03af1f91c0bf0f1e89
Brian DeCost <bdecost@andrew.cmu.edu>
2017-05-03 10:33:32 -0400

    replace spatial dimensions arguments with tensorflow dynamic shape.

--- uhcsseg/upsample.py
+++ uhcsseg/upsample.py
@@ -7,6 +7,11 @@ import numpy as np
 import tensorflow as tf
 from keras import backend as K
 
+def build_index(batch, x, y):
+    coords = tf.stack((batch, x, y), 2)
+    indices = tf.cast(tf.round(coords), tf.int32)
+    
+
 def get_values(inputs, **arguments):
     """ index into input tensor x with indices in input tensor sel
     indices should explicitly contain the sample index: (b, i, j)
@@ -14,11 +19,18 @@ def get_values(inputs, **arguments):
         needed since dimensions are not known at model compile time
     """
     data, coords = inputs
-
+    h = tf.cast(tf.shape(data)[1], tf.float32)
+    w = tf.cast(tf.shape(data)[2], tf.float32)
+    
     # transform fractional coordinates to feature map coordinates
-    x = coords[:,:,1] * arguments['h']
-    y = coords[:,:,2] * arguments['w']
-    coords = tf.stack((tf.zeros_like(x), x, y), 2)
+    batch = coords[:,:,0]
+    x = coords[:,:,1] * h
+    y = coords[:,:,2] * w
+    
+    x1, x2 = tf.floor(x), tf.ceil(x)
+    y1, y2 = tf.floor(y), tf.ceil(y)
+    
+    coords = tf.stack((batch, x, y), 2)
     indices = tf.cast(tf.round(coords), tf.int32)
 
     return tf.gather_nd(data, indices)
