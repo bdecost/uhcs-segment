13c48b0d83d7e7ba9678df2a34e72ee78e2feb4e
Brian DeCost <bdecost@andrew.cmu.edu>
2017-05-04 15:49:08 -0400

    clean up test/pixelnet_arch.py and fix coords -- should be fractional coordinates.

--- test/pixelnet_arch.py
+++ test/pixelnet_arch.py
@@ -3,17 +3,10 @@ import os
 import numpy as np
 import skimage.data
 import skimage.color
-from scipy.misc import imresize
-
-import tensorflow as tf
-from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, Lambda, Layer
-from keras import backend as K
 
 import sys
 sys.path.append(os.getcwd())
 
-from uhcsseg.upsample import sparse_upsample, sparse_upsample_output_shape
 from uhcsseg.pixelnet import pixelnet_model
 
 if __name__ == '__main__':
@@ -21,12 +14,12 @@ if __name__ == '__main__':
     im = skimage.color.rgb2gray(im)
 
     coords = np.random.random((1,2048,3))
-    coords *= np.array([0, im.shape[0], im.shape[1]])
-    coords = coords.astype(np.int32)
+    coords *= np.array([0, 1, 1])
+
+    print('coordinates to sample:')
     print(coords)
     
     model = pixelnet_model()
-
     f = model.predict([im[np.newaxis,:,:,np.newaxis], coords])
     print('sparse upsample:')
     print(f)
