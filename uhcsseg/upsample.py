f6a54d7f767c2877b62eef49d6f1c9f659a11081
Brian DeCost <bdecost@andrew.cmu.edu>
2017-05-03 22:43:06 -0400

    fix bilinear interpolation (except for last row/column of feature maps...)

--- uhcsseg/upsample.py
+++ uhcsseg/upsample.py
@@ -13,32 +13,42 @@ def get_values(data, batch, x, y):
     indices = tf.cast(coords, tf.int32)
     return tf.gather_nd(data, indices)    
 
+def offset(x1, x2):
+    """ helper for bilinear upsample: 
+    make offset tensor the same rank as data tensor for broadcasting.
+    """
+    dx = x2 - x1
+    return tf.expand_dims(dx, axis=-1)
+
 def sparse_upsample_bilinear(inputs, **arguments):
     """ upsample the input tensor `data` with indices in input tensor sel
     performs sparse bilinear interpolation
     indices should explicitly contain the sample index: (b, i, j)
     """
     data, coords = inputs
-    h = tf.cast(tf.shape(data)[1], tf.float32)
-    w = tf.cast(tf.shape(data)[2], tf.float32)
+    w = tf.cast(tf.shape(data)[1], tf.float32)
+    h = tf.cast(tf.shape(data)[2], tf.float32)
     
     # transform fractional coordinates to feature map coordinates
     batch = coords[:,:,0]
-    x = h * coords[:,:,1]
-    y = w * coords[:,:,2]
+    x = w * coords[:,:,1]
+    y = h * coords[:,:,2]
 
-    x1, x2 = tf.floor(x), tf.ceil(x)
-    y1, y2 = tf.floor(y), tf.ceil(y)
+    # clip feature map coordinates
+    # results in incorrect interpolation
+    # for samples in the last row and/or column
+    x1, y1 = tf.floor(x), tf.floor(y)
+    x1 = tf.clip_by_value(x1, 0, w-2)
+    y1 = tf.clip_by_value(y1, 0, h-2)
+    x2, y2 = 1 + x1, 1 + y1
 
     # horizontal interpolation first
-    top = (x2 - x) * get_values(data, batch, x1, y2)  + (x - x1) * get_values(data, batch, x2, y2)
-    bottom = (x2 - x) * get_values(data, batch, x1, y1)  + (x - x1) * get_values(data, batch, x2, y1)
+    top = get_values(data, batch, x1, y2) * offset(x, x2) +  get_values(data, batch, x2, y2) * offset(x1, x)
+    bottom = get_values(data, batch, x1, y1) * offset(x, x2)  + get_values(data, batch, x2, y1) * offset(x1, x)
 
     # vertical interpolation
-    interp = (y2 - y) * top +  (y - y1) * bottom
+    interp =  bottom * offset(y, y2) + top * offset(y1, y)
     return interp
-    # x, y = tf.round(x), tf.round(y)
-    # return get_values(data, batch, x, y)
 
 def sparse_upsample_nearest(inputs, **arguments):
     """ 'upsample' input tensor `data` with indices in input tensor sel
