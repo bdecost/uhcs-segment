6a3bbd75301134f3cef7be412f8df5788e49d885
Brian DeCost <bdecost@andrew.cmu.edu>
2017-06-15 15:51:13 -0400

    split out a separate function to load image and annotation for individual examples.

--- uhcsseg/data.py
+++ uhcsseg/data.py
@@ -2,22 +2,25 @@ import json
 import h5py
 import numpy as np
 
+def load_record(f, key, cropbar=None):
+    micrograph = f[key]
+    im = micrograph['image'][...]
+    l = micrograph['labels'][...]
+
+    if cropbar is not None and cropbar > 0:
+        # remove micron bar from bottom of image
+        im = im[:-cropbar]
+        l = l[:-cropbar]
+    return im, l
+
 def load_dataset(hfile, cropbar=None):
     """ load uhcsseg training data from hdf5 """
     
     images, labels, names = [], [], []
     with h5py.File(hfile, 'r') as f:
         for key in f:
-            micrograph = f[key]
-            im = micrograph['image'][...]
-            l = micrograph['labels'][...]
-
-            if cropbar is not None and cropbar > 0:
-                # remove micron bar from bottom of image
-                im = im[:-cropbar]
-                l = l[:-cropbar]
-                
-            names.append(key)        
+            im, l = load_record(f, key, cropbar=cropbar)   
+            names.append(key)       
             images.append(im)
             labels.append(l)
             
